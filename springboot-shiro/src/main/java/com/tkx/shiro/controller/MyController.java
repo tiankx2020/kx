7cc096af9bd79f25c7f4a6398c3246ec1f1656be
tiankx2020 <2633385761@qq.com>
2025-01-20 21:38:35 +0800

    shiro相关

--- springboot-shiro/src/main/java/com/tkx/shiro/controller/MyController.java
+++ springboot-shiro/src/main/java/com/tkx/shiro/controller/MyController.java
@@ -8,7 +8,8 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @Author tkx
@@ -18,10 +19,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/myController")
 public class MyController {
 
+    @GetMapping("/login")
+    public String log() {
+        return "login";
+    }
 
     @GetMapping("userLogin")
-    @ResponseBody
-    public String userLogin(String name,String pwd) {
+    public String userLogin(String name, String pwd, HttpSession session) {
         //1.获取subject对象
         Subject subject = SecurityUtils.getSubject();
         //2.封装请求数据到token
@@ -29,13 +33,14 @@ public class MyController {
         //3.调用login方法进行登录认证
         try {
             subject.login(token);
-            return "登录成功";
+            //return "登录成功";
+            session.setAttribute("user", token.getPrincipal().toString());
+            return "main";
         } catch (AuthenticationException e) {
             e.printStackTrace();
             System.out.println("登录失败");
             return "登录失败";
         }
     }
-
 
 }
