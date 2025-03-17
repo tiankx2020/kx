5b4d20dcb3e6b1608b1f39dd72bfbb488a44add7
tiankx2020 <2633385761@qq.com>
2025-03-17 23:04:19 +0800

    1

---
+++ base/src/main/java/com/tkx/BaseApplication.java
@@ -0,0 +1,19 @@
+package com.tkx;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Author tkx
+ * @Date 2025 03 17 20 54
+ **/
+@EnableAspectJAutoProxy
+@MapperScan("com.tkx.mapper")
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
---
+++ base/src/main/java/com/tkx/TransactionController.java
@@ -0,0 +1,39 @@
+// package com.tkx.base.controller;
+//
+// import com.tkx.controller.UserController;
+// import com.tkx.mapper.StudentMapper;
+// import com.tkx.mapper.UserMapper;
+// import com.tkx.pojo.Student;
+// import com.tkx.pojo.StudentDTO;
+// import com.tkx.pojo.User;
+// import com.tkx.service.StudentInterface;
+// import com.tkx.service.UserService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import javax.annotation.Resource;
+// import java.util.List;
+//
+// /**
+//  * @Author tkx
+//  * @Date 2025 03 17 20 59
+//  **/
+// @RestController
+// @RequestMapping("/tx")
+// public class TransactionController {
+//
+//     public static  int  v = 1;
+//
+//     @Autowired
+//     private StudentInterface studentInterface;
+//     @Transactional
+//     @PostMapping("/demo01")
+//     public void demo01(){
+//         System.out.println("======="+v);
+//         v++;
+//         studentInterface.getList(new StudentDTO());
+//     }
+// }
